0d441111b94b0032234b13d0cf959a9a24197625
yurii macbook pro <yurikorock777@gmail.com>
2025-09-26 20:20:37 +0300

    6-fetching-1

--- crm/src/app/(admin)/dashboard/@stats/page.tsx
+++ crm/src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { getSummaryStats } from '@/lib/api';
+import { getSummaryStats, SummaryStats } from '@/lib/api';
 import StatCard, { StatCardType } from '@/app/components/stat-card';
 import Link from 'next/link';
 
 export interface PageProps {}
 
-const labelByStat = {
+const labelByStat: Record<keyof SummaryStats, string> = {
   promotions: 'Total promotions',
   categories: 'Total categories',
   newCompanies: 'New companies',
@@ -17,7 +17,7 @@ export default async function Page({}: PageProps) {
 
   return (
     <div className="grid grid-cols-12 gap-5">
-      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
+      {(Object.keys(labelByStat) as (keyof SummaryStats)[]).map((key) => (
         <Link href={`/dashboard/${key}`} key={key} className="col-span-3">
           <StatCard
             type={StatCardType.Gradient}
